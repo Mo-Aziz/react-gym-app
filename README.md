5b2dfc5f7a2ba7e28f927c571265dff724407479
Mo-Aziz <mohamed.aref32@gmail.com>
2023-07-24 13:10:10 -0500

    finished app configuration and adding dependances

--- README.md
+++ README.md
@@ -11,10 +11,23 @@ In the project directory, you can run:
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## dependancies
 
-### `npm test`
+### **\*\*\*\***\*\*\*\***\*\*\*\***
+
+### "@emotion/react": "^11.9.0",
+
+### "@emotion/styled": "^11.8.1",
+
+### "@mui/icons-material": "^5.6.1",
+
+### "@mui/material": "^5.6.1",
+
+### "react-horizontal-scrolling-menu": "^2.7.1",
+
+### "react-loader-spinner": "^6.0.0-0",
+
+###
 
 Launches the test runner in the interactive watch mode.\
 See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Box from "@mui/material/Box";
+
+// import { Home, About, Events, Contact, Whoops404 } from "./pages";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box width="400px" sx={{ width: { xl: "1488" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 }
 
